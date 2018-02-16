dd26863d6ec0ebacdbf60e9488d55381f2d0a1d7
Oleg Akbarov <olegakbarov@gmail.com>
2018-02-16 12:52:50 +0300

    moved ntf to separate component

--- src/app/components/header.cljs
+++ src/app/components/header.cljs
@@ -3,47 +3,8 @@
             [app.db :refer [db router]]
             [app.actions.ui :refer [to-screen]]
             [app.motion :refer [Motion spring presets]]
+            [app.components.header :refer [ntf]]
             [goog.object :as gobj]))
-
-(def height 30)
-
-(defn ntf
-  [n]
-  (let [{:keys [color text]} (-> @db
-                                 :ui/ntf)]
-    (when (and text color) [:div {:style {:background-color color}} text])))
-
-(defn view
-  [{c :children}]
-  (let [y (gobj/get c "y")]
-    [:div
-     {:style {:position "fixed"
-              :width "100%"
-              :height (str height "px")
-              :line-height (str height "px")
-              :color "#fff"
-              :z-index 100
-              :text-align "center"
-              :font-size "12px"
-              :-webkit-transform (str "translateY(" y "px)")
-              :transform (str "translateY(" y "px)")}}
-     [ntf]]))
-
-(def animated-comp (reagent/reactify-component view))
-
-(defn ntf-view
-  []
-  (fn []
-    [:div
-     {:style {:position "absolute"
-              :bottom 0
-              :height (str height "px")
-              :width "100%"
-              :z-index 100
-              :display (if (:ui/ntf @db) "block" "none")}}
-     [Motion
-      {:style {:y (spring (if (:ui/ntf @db) height 0))}}
-      (fn [x] (reagent/create-element animated-comp #js {} x))]]))
 
 (defn nav
   []
@@ -76,4 +37,4 @@
                  :position "absolute"
                  :z-index "101"}}]
        [:div {:style {:z-index 101}} [:div.title "1.0.0-beta.1"] [nav]]
-       [ntf-view]])))
+       [ntf]])))
--- src/app/components/ntf.cljs
+++ src/app/components/ntf.cljs
@@ -1,2 +1,42 @@
 (ns app.components.ntf
   (:require [app.db :refer [db]]))
+
+(def height 30)
+
+(defn ntf-comp
+  [n]
+  (let [{:keys [color text]} (-> @db
+                                 :ui/ntf)]
+    (when (and text color) [:div {:style {:background-color color}} text])))
+
+(defn view
+  [{c :children}]
+  (let [y (gobj/get c "y")]
+    [:div
+     {:style {:position "fixed"
+              :width "100%"
+              :height (str height "px")
+              :line-height (str height "px")
+              :color "#fff"
+              :z-index 100
+              :text-align "center"
+              :font-size "12px"
+              :-webkit-transform (str "translateY(" y "px)")
+              :transform (str "translateY(" y "px)")}}
+     [ntf-comp]]))
+
+(def animated-comp (reagent/reactify-component view))
+
+(defn ntf
+  []
+  (fn []
+    [:div
+     {:style {:position "absolute"
+              :bottom 0
+              :height (str height "px")
+              :width "100%"
+              :z-index 100
+              :display (if (:ui/ntf @db) "block" "none")}}
+     [Motion
+      {:style {:y (spring (if (:ui/ntf @db) height 0))}}
+      (fn [x] (reagent/create-element animated-comp #js {} x))]]))
