9417ba82bfebace093279e8f1e82c94e68837d81
Oleg Akbarov <olegakbarov@gmail.com>
2018-02-18 19:18:10 +0300

    added dev namespace, should fix Travis

---
+++ dev/dev.cljs
@@ -0,0 +1,17 @@
+(ns dev
+  (:require
+    [clojure.pprint :refer [pprint]]
+    [mount.core :as mount]
+    [app.renderer]))
+
+(enable-console-print!)
+
+(defn start []
+  (mount/start))
+
+(defn go []
+  (mount/stop))
+
+(defn reset []
+  (go))
+
